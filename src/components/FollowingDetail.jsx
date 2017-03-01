f482a0dc136c94cb1eaac6f227c47c11f1c13265
Evgeny Morozov <evmorov@gmail.com>
2017-03-02 00:02:24 +0300

    FollowingDetail is a class

--- src/components/FollowingDetail.jsx
+++ src/components/FollowingDetail.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserDetail from './UserDetail';
 
-const FollowingDetail = ({following, octo}) => {
-  if (!following) return <div>Loading...</div>;
+class FollowingDetail extends Component {
 
-  return (
-    <div id="following-detail" className="col-md-10">
-      <h1>{following.login}</h1>
-      <img className="img-responsive" src={following.avatarUrl} alt={following.login} />
-      <a href={following.htmlUrl}>GitHub link</a>
-      <UserDetail octo={octo} following={following} />
-    </div>
-  );
-};
+  render() {
+    const following = this.props.following;
+    if (!following) return <div>Loading...</div>;
+
+    return (
+      <div id="following-detail" className="col-md-10">
+        <h1>{following.login}</h1>
+        <img className="img-responsive" src={following.avatarUrl} alt={following.login} />
+        <a href={following.htmlUrl}>GitHub link</a>
+        <UserDetail octo={this.props.octo} following={following} />
+      </div>
+    );
+  }
+}
 
 export default FollowingDetail;
