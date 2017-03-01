15fabc98b8e5804d8272bd8243d4645168ac9738
Evgeny Morozov <evmorov@gmail.com>
2017-03-01 16:34:52 +0300

    Change navbar color

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,7 +22,7 @@ class Header extends Component {
     }
 
     return (
-      <Navbar inverse>
+      <Navbar>
         <Grid>
           <Navbar.Header>
             <Navbar.Brand>YouFollow</Navbar.Brand>
