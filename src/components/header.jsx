bb54c068904d9a1f526b51f79c8d424d2fa7119e
Evgeny Morozov <evmorov@gmail.com>
2017-03-01 17:33:55 +0300

    Header from class to function

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HeaderUser from './HeaderUser';
 import HeaderGuest from './HeaderGuest';
 import { Grid, Navbar } from 'react-bootstrap';
 
-class Header extends Component {
-  render() {
-    let authLinks = null;
-    if (this.props.me) {
-      authLinks = <HeaderUser me={this.props.me} clearAppState={this.props.clearAppState} />
-    } else {
-      authLinks = <HeaderGuest />
-    }
-
-    return (
-      <Navbar>
-        <Grid>
-          <Navbar.Header>
-            <Navbar.Brand>YouFollow</Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          {authLinks}
-        </Grid>
-      </Navbar>
-    );
+const Header = ({ me, clearAppState }) => {
+  let authLinks = null;
+  if (me) {
+    authLinks = <HeaderUser me={me} clearAppState={clearAppState} />;
+  } else {
+    authLinks = <HeaderGuest />;
   }
+
+  return (
+    <Navbar>
+      <Grid>
+        <Navbar.Header>
+          <Navbar.Brand>YouFollow</Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        {authLinks}
+      </Grid>
+    </Navbar>
+  );
 }
 
 export default Header;
