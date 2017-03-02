88d3c2267c22226f1e7031296ba9bb3dc7206605
Evgeny Morozov <evmorov@gmail.com>
2017-03-02 14:23:27 +0300

    Show relative date for repos

--- src/components/RepoListItem.jsx
+++ src/components/RepoListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ListGroupItem } from 'react-bootstrap';
+import Moment from 'moment';
 
 const RepoListItem = ({ repo }) => {
   return (
@@ -10,7 +11,7 @@ const RepoListItem = ({ repo }) => {
         <span>Forks: {repo.forksCount} | </span>
         <span>Stars: {repo.stargazersCount} | </span>
         <span>Watchers: {repo.watchersCount} | </span>
-        <span>Updated at: {repo.updatedAt.toString()}</span>
+        <span>Updated: {Moment(repo.updatedAt).fromNow()}</span>
       </div>
       <div><a href={repo.htmlUrl}>GitHub link</a></div>
     </ListGroupItem>
